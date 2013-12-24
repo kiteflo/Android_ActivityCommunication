f7878aaee338b79328aa533753089cbc98584278
kiteflo81 <kiteflo@mac.com>
2013-12-24 10:04:03 +0100

    Created clean base structure and added first fragment UI components

---
+++ ActivityCommunication/src/main/java/com/jooik/training/FragmentMain.java
@@ -0,0 +1,19 @@
+package com.jooik.training;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentMain extends Fragment {
+
+    private View mView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_main, container, false);
+        return mView;
+    }
+}
